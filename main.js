6eba0e047741a3da267ec2aba24d5eb0c573632c
VJ V. Pajo <vjpajo67@gmail.com>
2025-12-12 22:56:10 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
+// THEME TOGGLE
 const toggle = document.getElementById("themeToggle");
 
 toggle.addEventListener("click", () => {
     document.body.classList.toggle("dark");
-
-    
     toggle.textContent = document.body.classList.contains("dark") ? "🌙" : "☀";
 });
